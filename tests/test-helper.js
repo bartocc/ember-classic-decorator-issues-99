51fda73bab2701409612d341b07f2e0ef809c8b9
Julien Palmas <julien@crx.io>
2022-11-07 09:31:29 +0100

    initial

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'foo/app';
+import config from 'foo/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
