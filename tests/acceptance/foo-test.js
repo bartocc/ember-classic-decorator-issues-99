6dcf19cb3c05d2dc9a5b00d3616aca29b06e87f9
Julien Palmas <julien@crx.io>
2022-11-07 15:41:43 +0100

    minimal repro

--- tests/acceptance/foo-test.js
+++ tests/acceptance/foo-test.js
@@ -1,13 +1,12 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'foo/tests/helpers';
+import { isTesting } from '@embroider/macros/runtime';
 
 module('Acceptance | foo', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /foo', async function (assert) {
-    await visit('/foo');
-
-    assert.strictEqual(currentURL(), '/foo');
+  test('Check isTesting from @embroider/macros/runtime', async function (assert) {
+    assert.ok(isTesting());
   });
 });
